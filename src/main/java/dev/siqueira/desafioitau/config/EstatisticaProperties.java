40a6f929223eaf39a2ecb2c1ae9eb18fca46e920
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2026-03-10 18:40:20 -0300

    Feat: Adição de validação, para caso o valor de segundo seja maior que zero e positivo
    adição das libs Swagger e JUnit

--- src/main/java/dev/siqueira/desafioitau/config/EstatisticaProperties.java
+++ src/main/java/dev/siqueira/desafioitau/config/EstatisticaProperties.java
@@ -1,7 +1,12 @@
 package dev.siqueira.desafioitau.config;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer segundos) {
+public record EstatisticaProperties(@NotNull @Positive @Min(1) Integer segundos) {
 }
