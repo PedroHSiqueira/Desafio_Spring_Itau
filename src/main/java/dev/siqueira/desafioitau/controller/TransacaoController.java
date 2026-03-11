806221d7ed73605219501f7e0fa7563ccd167998
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2026-03-11 19:02:45 -0300

    Feat: Adição da documentação (Swagger TAG's)

--- src/main/java/dev/siqueira/desafioitau/controller/TransacaoController.java
+++ src/main/java/dev/siqueira/desafioitau/controller/TransacaoController.java
@@ -4,6 +4,9 @@ import dev.siqueira.desafioitau.Entity.Trasacao;
 import dev.siqueira.desafioitau.dto.TransacaoRequest;
 import dev.siqueira.desafioitau.repository.TrasacaoRepository;
 import dev.siqueira.desafioitau.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/transacao")
+@Tag(name="Transações", description = "Endpoints para criação e deleção de Transações")
 public class TransacaoController {
 
     private final TransacaoService transacaoService;
@@ -23,6 +27,10 @@ public class TransacaoController {
     }
 
     @PostMapping
+    @Operation(summary = "Cria transações", description = "Recebe uma transação valida e adiciona na lista")
+    @ApiResponse(responseCode = "201", description = "Transação realizada com sucesso!")
+    @ApiResponse(responseCode = "422", description = "Transação mal formatada!")
+    @ApiResponse(responseCode = "400", description = "Erro no servidor")
     public ResponseEntity<Trasacao> adicionar(@RequestBody TransacaoRequest trasacao){
         try {
             transacaoService.validar(trasacao);
@@ -35,7 +43,6 @@ public class TransacaoController {
             log.error("Ocorreu um erro na passagem dos parametros para formularios! tente novamente");
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         } catch (Exception e) {
-
             log.error("Ocorreu um erro interno no sistema! tente novamente");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
